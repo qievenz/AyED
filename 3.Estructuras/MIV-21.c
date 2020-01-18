a673c43a816988cc5330ed9a1cad7b5ac934f126
qievenz <ivann96@gmail.com>
2020-01-18 16:34:14 -0300

    Final

--- 3.Estructuras/MIV-21.c
+++ 3.Estructuras/MIV-21.c
@@ -1,3 +1,46 @@
 //Una empresa de micros de larga distancia necesita armar  la frecuencia necesaria para satisfacer la demanda de pasajeros.  En un archivo se tienen  las paradas-estaciones  con sus nombres  junto con los movimientos de pasajeros en lo que hace a  ascenso  y descenso en cada una de ellas en ambas direcciones.
 //Un micro de 50 asientos sale desde el origen al destino pasando por las paradas y retorna  al origen.
 //Indicar en que estaciones y sentido se verá saturado.
+#include <stdio.h>
+#include <stdlib.h>
+#define LOG(x) printf(x)
+
+typedef struct estacion
+{
+    char nombre[30];
+    char direccion[1]; //Hacia el destino: 'D'. Hacia el origen: 'O'
+    int cantAscenso;
+    int cantDescenso;
+    struct estacion *ptrSiguiente;
+} Estacion, *ptrEstacion;
+
+int main(int argc, char const *argv[])
+{
+    /*codigo*/
+    return 0;
+}
+
+void imprimirDondeSeSatura(ptrEstacion ptrInicial)
+{
+    int totalAsientos = 50;
+    int cantPasajeros = 0;
+    ptrEstacion ptrEstacionActual = ptrInicial;
+    while (ptrEstacionActual != NULL)
+    {
+        cantPasajeros = cantPasajeros + ptrEstacionActual->cantAscenso - ptrEstacionActual->cantDescenso;
+        if (cantPasajeros > totalAsientos)
+        {
+            printf("Saturacion en estacion: %s\n", ptrEstacionActual->nombre);
+            printf("Cantidad de pasajeros: %d\n", cantPasajeros);
+            if (ptrEstacionActual->direccion[0] == 'D')
+            {
+                printf("Direccion: llendo al destino\n");
+            }
+            else
+            {
+                printf("Direccion: llendo al origen\n");
+            }
+        }
+        ptrEstacionActual = ptrEstacionActual->ptrSiguiente;
+    }
+}
---
+++ 99.Finales/Final AyED - 11-02-2019.c
@@ -0,0 +1,56 @@
+/* comentario */
+#include <stdio.h>
+#include <stdlib.h>
+#define LOG(x) printf(x)
+
+typedef struct cliente
+{
+    int nroCarrito;
+    struct cliente *ptrSiguiente;
+} Cliente, *ptrCliente;
+
+typedef struct caja
+{
+    int numeroCaja;
+    float efectivo;
+    int cantCuponesTC;
+    int impTC;
+    int cantCuponesTD;
+    struct cliente *ptrClienteAsignado;
+} Caja, *ptrCaja;
+
+void queue(ptrCliente ptrFinal);
+void unQueue(ptrCliente ptrInicial);
+
+int main(int argc, char const *argv[])
+{
+    /*codigo*/
+    return 0;
+}
+
+ptrCaja CajasHabilitadas(FILE *archivo)
+{
+    Caja cajas[40];
+    archivo = fopen("Cajas.dat", "r");
+    for (int i = 0; i < 40; i++)
+    {
+        fscanf(archivo, "%d %d", &cajas[i].numeroCaja, &cajas[i].efectivo);
+        cajas[i].ptrClienteAsignado = NULL;
+    }
+    fclose(archivo);
+    return cajas;
+}
+
+void nuevoCliente(ptrCliente ptrFinal, int nroCarrito)
+{
+    ptrCliente ptrNuevo = (ptrCliente)malloc(sizeof(Cliente));
+    ptrNuevo->nroCarrito = nroCarrito;
+    ptrFinal->ptrSiguiente = ptrNuevo;
+    ptrFinal = ptrNuevo;
+}
+
+void AtenderCliente(Caja cajas[], int nroCaja, ptrCliente ptrClienteInicio)
+{
+    cajas[nroCaja].ptrClienteAsignado = ptrClienteInicio;
+    calcularImportes(int nroCarrito, int efectivo, int tc, int td, int ticket);
+}
