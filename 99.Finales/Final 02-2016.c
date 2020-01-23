1afb8b60d3588a0c023297f4644a5608c02cb7d1
qievenz <ivann96@gmail.com>
2020-01-23 19:44:00 -0300

    Final 02-2016

---
+++ 99.Finales/Final 02-2016.c
@@ -0,0 +1,14 @@
+/* comentario */
+#include <stdio.h>
+#include <stdlib.h>
+#define LOG(x) printf(x)
+
+/*prototipo*/
+
+int main(int argc, char const *argv[])
+{
+    /*codigo*/
+    return 0;
+}
+
+/*funcion*/
