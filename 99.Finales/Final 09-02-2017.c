f266fdd8cdaf2dc24196e66df760f44ed207093b
qievenz <ivann96@gmail.com>
2020-02-03 15:21:25 -0300

    recursantes

--- 99.Finales/Final 09-02-2017.c
+++ 99.Finales/Final 09-02-2017.c
@@ -11,7 +11,7 @@ estructura enlazada -> array [7]
 */
 #include <stdio.h>
 #include <stdlib.h>
-#define LOG(x) printf(x)
+//#define LOG(x) printf(x)
 
 typedef struct instancia
 {
@@ -27,10 +27,10 @@ typedef struct instanciaAlumno
 {
     int numeroLegajo;
     char apellidoNombre[30];
-    InstanciaParcial instancia;
+    int instancia;
     int nota;
     char texto[50];
-    ptrInstanciaAlumno ptrSiguiente;
+    struct ptrInstanciaAlumno *ptrSiguiente;
 } InstanciaAlumno, *ptrInstanciaAlumno;
 
 void inicializarArrInstancia(InstanciaAlumno arrInstancia[7]);
@@ -41,36 +41,81 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void recursantes(FILE *archivo, ptrInstanciaAlumno ptrRecursantes)
+void recursantes(FILE *archivo)
 { //Se considera el archivo abierto y con datos
-    InstanciaAlumno alumno;
-    InstanciaAlumno alumnoAnterior;
-    InstanciaAlumno arrAlumno[7];
+    ptrInstanciaAlumno ptrResultado = NULL;
+    ptrInstanciaAlumno ptrAlumno;
+    ptrInstanciaAlumno ptrRecursante = NULL;
+    InstanciaParcial instancia;
+    ptrInstanciaAlumno ptrAlumnoAValidarInicio;
+    ptrAlumno = (ptrInstanciaAlumno)malloc(sizeof(InstanciaAlumno));
+    fscanf(archivo, "%d %s %d %d %s", ptrAlumno->numeroLegajo, ptrAlumno->apellidoNombre, ptrAlumno->instancia, ptrAlumno->nota, ptrAlumno->texto);
+    ptrAlumno->ptrSiguiente = NULL;
+    //ptrRecursantes = ptrAlumno;
+    ptrAlumnoAValidarInicio = ptrAlumno;
+    ptrInstanciaAlumno ptrAlumnoAnterior;
+    ptrAlumnoAnterior = ptrAlumno;
+    int aprobo_parcial_1 = 0;
+    int aprobo_parcial_2 = 0;
 
-    int i = 0;
-    inicializarArrInstancia(arrAlumno);
-    //fscanf(archivo, &arrAlumno[i]);
-    fscanf(archivo, "%d %s %d %d %s", &alumno.numeroLegajo, alumno.apellidoNombre, &alumno.instancia.parcial_1, &alumno.nota, alumno.texto);
     while (feof(archivo))
     {
-        i = 0;
-        alumnoAnterior = arrAlumno[i];
-        while (arrAlumno[i].numeroLegajo == alumnoAnterior.numeroLegajo)
+        ptrAlumno = (ptrInstanciaAlumno)malloc(sizeof(InstanciaAlumno));
+        fscanf(archivo, "%d %s %d %d %s", ptrAlumno->numeroLegajo, ptrAlumno->apellidoNombre, ptrAlumno->instancia, ptrAlumno->nota, ptrAlumno->texto);
+
+        if (ptrAlumno->instancia == 1)
         {
-            alumnoAnterior = arrAlumno[i];
-            i++;
-            fscanf(archivo, &arrAlumno[i]);
+            instancia.parcial_1 = ptrAlumno->nota;
         }
-        //Validar notas
-        int promedio = 0;
-        for (int j = 0; j < i; j++)
+        else if (ptrAlumno->instancia == 11)
         {
+            instancia.parcial_1_rec_1 = ptrAlumno->nota;
+        }
+        else if (ptrAlumno->instancia == 12)
+        {
+            instancia.parcial_1_rec_2 = ptrAlumno->nota;
+        }
+        else if (ptrAlumno->instancia == 2)
+        {
+            instancia.parcial_2 = ptrAlumno->nota;
+        }
+        else if (ptrAlumno->instancia == 21)
+        {
+            instancia.parcial_2_rec_1 = ptrAlumno->nota;
+        }
+        else if (ptrAlumno->instancia == 22)
+        {
+            instancia.parcial_1_rec_2 = ptrAlumno->nota;
         }
 
-        //Resetear array
-        alumnoAnterior = arrAlumno[i];
-        inicializarArrInstancia(arrAlumno);
-        arrAlumno[0] = alumnoAnterior;
+        if (ptrAlumnoAnterior->numeroLegajo != ptrAlumno->numeroLegajo)
+        {
+            //Validar instancias
+            if (instancia.parcial_1 >= 6 || instancia.parcial_1_rec_1 >= 6 || instancia.parcial_1_rec_2 >= 6)
+            {
+                aprobo_parcial_1 = 1;
+            }
+            if (instancia.parcial_2 >= 6 || instancia.parcial_2_rec_1 >= 6 || instancia.parcial_2_rec_2 >= 6)
+            {
+                aprobo_parcial_2 = 1;
+            }
+            if (aprobo_parcial_1 == 0 && aprobo_parcial_2 == 0)
+            {
+                if (ptrResultado == NULL)
+                {
+                    ptrResultado = ptrAlumno;
+                    ptrResultado->ptrSiguiente = NULL;
+                    ptrRecursante = ptrResultado;
+                }
+                else
+                {
+                    ptrRecursante->ptrSiguiente = ptrAlumno;
+                    ptrRecursante = ptrAlumno;
+                }
+            }
+        }
+        ptrAlumnoAnterior->ptrSiguiente = ptrAlumno;
+        ptrAlumnoAnterior = ptrAlumno;
     }
 }
 
