68bc0d3614becdf89095a14dc6644ab3e6b52401
Iván Velázquez <ivann96@gmail.com.com>
2020-02-08 22:50:12 -0300

    final

--- 99.Finales/Final 23-02-2017.c
+++ 99.Finales/Final 23-02-2017.c
@@ -1,13 +1,29 @@
+/* comentario */
+#include <stdio.h>
+#include <stdlib.h>
+//#define LOG(x) printf(x)
+
 typedef struct rNota
 {
     int nota;
     struct rNota *ptrSiguiente;
+};
+typedef struct NodoNota
+{
+    struct rNota info;
+    struct NodoNota *ptrSiguiente;
 };
 
 typedef struct rEstudiante
 {
     int id;
     struct rNota notas[4];
+};
+
+typedef struct NodoEstudiante
+{
+    struct rEstudiante info;
+    struct NodoEstudiante *ptrSiguiente;
 };
 
 typedef struct rCurso
@@ -20,5 +36,19 @@ typedef struct rCurso
 typedef struct NodoCurso
 {
     struct rCurso info;
-    struct rCurso *ptrSiguiente;
-};
+    struct NodoCurso *ptrSiguiente;
+} NodoCurso;
+
+void ProcesarNovedades(FILE *archivoNovedades, NodoCurso *ptrCursosInicio);
+
+int main(int argc, char const *argv[])
+{
+    /*codigo*/
+    return 0;
+}
+
+void ProcesarNovedades(FILE *archivoNovedades, NodoCurso *ptrCursosInicio)
+{
+    archivoNovedades = fopen("archivo.dat", "r");
+    fscanf(archivoNovedades, "")
+}
